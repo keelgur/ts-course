e99ba58e2d5023e7a1dae6915eaa5608cb6f67c2
Keel Gur <eblan4len1488@gmail.com>
2026-02-10 16:30:17 +0200

    Added homework4

---
+++ hm4/homework4.ts
@@ -0,0 +1,67 @@
+class Circle {
+  public readonly color: string;
+  public readonly name: string;
+
+  constructor(name: string, color: string) {
+    this.color = color;
+    this.name = name;
+  }
+
+  public calculateArea(): number {
+    //TODO: calculateArea(); There's no instructions to fully implement radius/height/other properties to get area
+    return -1;
+  }
+}
+
+class Triangle {
+  public readonly color: string;
+  public readonly name: string;
+
+  constructor(name: string, color: string) {
+    this.color = color;
+    this.name = name;
+  }
+
+  public calculateArea(): number {
+    //TODO: calculateArea(); There's no instructions to fully implement radius/height/other properties to get area
+    return -1;
+  }
+}
+
+class Rectangle {
+  public readonly color: string;
+  public readonly name: string;
+
+  constructor(name: string, color: string) {
+    this.color = color;
+    this.name = name;
+  }
+
+  print(): void {
+    console.log("S=a*b");
+  }
+
+  public calculateArea(): number {
+    //TODO: calculateArea(); There's no instructions to fully implement radius/height/other properties to get area
+    return -1;
+  }
+}
+
+class Square {
+  public readonly color: string;
+  public readonly name: string;
+
+  constructor(name: string, color: string) {
+    this.color = color;
+    this.name = name;
+  }
+
+  print(): void {
+    console.log("S=a*a");
+  }
+
+  public calculateArea(): number {
+    //TODO: calculateArea(); There's no instructions to fully implement radius/height/other properties to get area
+    return -1;
+  }
+}
