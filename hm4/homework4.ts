d18ea8278ee66a0ea81fef78653b54e85edd4547
Keel Gur <eblan4len1488@gmail.com>
2026-02-10 18:52:28 +0200

    Fixed homework4

--- hm4/homework4.ts
+++ hm4/homework4.ts
@@ -1,4 +1,8 @@
-class Circle {
+interface PrintProcessor {
+  print(): void;
+}
+
+abstract class Figure {
   public readonly color: string;
   public readonly name: string;
 
@@ -7,61 +11,66 @@ class Circle {
     this.name = name;
   }
 
-  public calculateArea(): number {
-    //TODO: calculateArea(); There's no instructions to fully implement radius/height/other properties to get area
-    return -1;
+  public abstract calculateArea(): number;
+}
+
+class Circle extends Figure {
+  private _radius: number;
+  constructor(name: string, color: string, r: number) {
+    super(name, color);
+    this._radius = r;
+  }
+
+  calculateArea(): number {
+    return Math.pow(this._radius, 2) * Math.PI;
   }
 }
 
-class Triangle {
-  public readonly color: string;
-  public readonly name: string;
-
-  constructor(name: string, color: string) {
-    this.color = color;
-    this.name = name;
+class Triangle extends Figure {
+  private _height: number;
+  private _side: number;
+  constructor(name: string, color: string, a: number, h: number) {
+    super(name, color);
+    this._height = h;
+    this._side = a;
   }
 
-  public calculateArea(): number {
-    //TODO: calculateArea(); There's no instructions to fully implement radius/height/other properties to get area
-    return -1;
+  calculateArea(): number {
+    return (this._height * this._side) / 2;
   }
 }
 
-class Rectangle {
-  public readonly color: string;
-  public readonly name: string;
-
-  constructor(name: string, color: string) {
-    this.color = color;
-    this.name = name;
+class Rectangle extends Figure implements PrintProcessor {
+  private _a: number;
+  private _b: number;
+  constructor(name: string, color: string, a: number, b: number) {
+    super(name, color);
+    this._a = a;
+    this._b = b;
   }
 
   print(): void {
     console.log("S=a*b");
   }
 
-  public calculateArea(): number {
-    //TODO: calculateArea(); There's no instructions to fully implement radius/height/other properties to get area
-    return -1;
+  calculateArea(): number {
+    return this._a * this._b;
   }
 }
 
-class Square {
-  public readonly color: string;
-  public readonly name: string;
+class Square extends Figure implements PrintProcessor {
+  private _a: number;
 
-  constructor(name: string, color: string) {
-    this.color = color;
-    this.name = name;
+  constructor(name: string, color: string, a: number) {
+    super(name, color);
+    this._a = a;
   }
 
   print(): void {
     console.log("S=a*a");
   }
 
-  public calculateArea(): number {
-    //TODO: calculateArea(); There's no instructions to fully implement radius/height/other properties to get area
-    return -1;
+  calculateArea(): number {
+    return this._a * this._a;
   }
 }
